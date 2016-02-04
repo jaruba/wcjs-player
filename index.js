d6b379cbc1bb8f4e0ef48e606765e092d918d64b
jaruba <branza.alex@gmail.com>
2016-02-04 23:54:49 +0200

    v0.5.5

--- index.js
+++ index.js
@@ -16,7 +16,7 @@
 * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
 *****************************************************************************/
 
-// WebChimera.js Player v0.5.4
+// WebChimera.js Player v0.5.5
 
 var vlcs = {},
     opts = {},
@@ -64,7 +64,7 @@ window.onbeforeunload = function(e) {
 
 function wjs(context) {
 
-    this.version = "v0.5.4";
+    this.version = "v0.5.5";
 
     // Save the context
     this.context = (typeof context === "undefined") ? "#webchimera" : context;  // if no playerid set, default to "webchimera"
